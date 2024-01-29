a83cddc82d0741be3beb7afb6ed3f4e58ba53ac9
katieblumenow2123 <146842677+katieblumenow2123@users.noreply.github.com>
2024-01-29 15:00:26 +0000

    Task1

--- tasks/task.c
+++ tasks/task.c
@@ -7,26 +7,24 @@
  */
 int main(int argc, char *argv[]) 
 {
-    if (argc != 3)
-    {
-        printf("usage: wlk <num1>\n");
+    if (argc != 3){
+        printf("usage: task <name>\n");
         return 0;
     }
+
+
+    int sum = atoi(argv[1])+ atoi(argv[2]);
+    if (sum==0){
+        printf("usage: task <name>\n");
+        return 0;
+    }
+    printf("%d\n", sum);
+
         
-    int sum = atoi(argv[1]) + atoi(argv[2])
-    printf("%s",sum);
 
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
 
-    // Calculate and print the sum
-    int sum = num1 + num2;
-    printf("Sum: %d\n", sum);
 
 
     return 0; // Exit successfully
